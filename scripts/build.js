7bd07e224646144581b86ad18cd604da92b6e486
MichelML <michmoreau.l@gmail.com>
2018-09-15 12:14:46 -0400

    fix demo

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
-process.env.PUBLIC_URL = 'https://raw.githubusercontent.com/MichelML/etf-investing-prep/master/docs/';
+process.env.PUBLIC_URL = 'https://cdn.rawgit.com/MichelML/etf-investing-prep/master/docs/';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
