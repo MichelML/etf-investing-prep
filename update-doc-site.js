3219a6896512def01d42834d3fe3344445d264e3
MichelML <michmoreau.l@gmail.com>
2018-09-15 03:27:35 -0400

    add script to update doc site

--- update-doc-site.js
+++ update-doc-site.js
@@ -1,7 +1,6 @@
 const execSync = require('child_process').execSync;
 const originWithAuthentication = `https://${process.env.u}:${process.env.p}@github.com/coveo/react-vapor.git`;
 
-execSync(`git --config `)
 execSync('git add .');
 execSync(`git commit -m "update doc site ${Date.now()}"`);
 execSync(`git push ${originWithAuthentication} master`);
