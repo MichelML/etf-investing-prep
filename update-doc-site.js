a609f3330869987059b0ac42353bf4fec0b4cdac
MichelML <michmoreau.l@gmail.com>
2018-09-15 03:32:10 -0400

    add script to update doc site

--- update-doc-site.js
+++ update-doc-site.js
@@ -1,5 +1,5 @@
 const execSync = require('child_process').execSync;
-const originWithAuthentication = `https://${process.env.u}:${process.env.p}@github.com/coveo/react-vapor.git`;
+const originWithAuthentication = `https://${process.env.u}:${process.env.p}@github.com/MichelML/etf-investing-prep.git`;
 
 execSync(`git config --global user.email "${process.env.e}"`)
 execSync(`git config --global user.name "${process.env.u}"`)
